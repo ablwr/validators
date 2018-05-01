fba5cf7aae5e8a5d30d01ed68d1d80596447e954
Ashley Blewer <ashley.blewer@gmail.com>
2018-05-01 15:12:41 -0400

    init commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var validate = document.getElementById('validate');
+
+var schemaData;
+
+fetch('pbcore-2.1.xsd')
+  .then(response => response.text())
+  .then(data => schemaData = data)
+
+validate.addEventListener('click', function(e){
+  var Params = {
+    xml: document.getElementById('xmlData').value,
+    schema: schemaData,
+    arguments: ["--noout", "--schema", "pbcore-2.1.xsd", "input.xml"]
+  };
+  var result = validateXML(Params);
+  if (result.search("xml validates") > 0) {
+    document.getElementById('resultConsole').innerHTML = "Valid!"
+  } else {
+    document.getElementById('resultConsole').innerHTML = ("NOT so valid!\n\n" + result)
+  }
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+# pbcore-validator
+
+
+Inspo comes straight from this [xml.js](https://github.com/kripken/xml.js) demo.
